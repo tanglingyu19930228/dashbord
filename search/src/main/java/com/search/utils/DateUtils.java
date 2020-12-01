b4145c6003f305e7008cd86d4570448a7d72575a
duoduoCain <xuxiaoby@qq.com>
2020-12-01 13:52:31 +0800

    some utils

---
+++ search/src/main/java/com/search/utils/DateUtils.java
@@ -0,0 +1,8 @@
+package com.search.utils;
+
+/**
+ * @author Administrator
+ */
+public class DateUtils  extends org.apache.commons.lang3.time.DateUtils {
+
+}
---
+++ search/src/main/java/com/search/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.search.utils;
+
+/**
+ * @author Administrator
+ */
+public class StringUtils  extends org.apache.commons.lang3.StringUtils {
+
+
+
+}
